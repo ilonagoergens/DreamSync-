7df569c953bd5f11e639777cb0eef27c7911eda5
ilonagoergens <ilona.goergens@edu.techstarter.de>
2025-03-11 21:09:46 +0100

    add: main.tf for s3 bucket

--- infra/main.tf
+++ infra/main.tf
@@ -3,3 +3,45 @@ provider "aws" {
   region = var.aws_region
 }
 
+resource "aws_s3_bucket" "website_bucket" {
+  bucket = var.bucket_name
+
+  website {
+    index_document = "index.html"
+  }
+}
+
+# Blockieren des öffentlichen Zugriffs auf den S3-Bucket
+resource "aws_s3_bucket_public_access_block" "public_access" {
+  bucket = aws_s3_bucket.website_bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+# S3 Bucket Policy, um den Zugriff auf die index.html-Datei zu ermöglichen
+resource "aws_s3_bucket_policy" "bucket_policy" {
+  bucket = aws_s3_bucket.website_bucket.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect    = "Allow",
+        Principal = "*",
+        Action    = "s3:GetObject",
+        Resource  = "${aws_s3_bucket.website_bucket.arn}/*"
+      }
+    ]
+  })
+}
+
+# Hochladen der index.html-Datei
+resource "aws_s3_object" "index_html" {
+  bucket       = aws_s3_bucket.website_bucket.bucket
+  key          = "index.html"
+  source       = "../app/index.html"
+  content_type = "text/html"
+}
--- infra/variables.tf
+++ infra/variables.tf
@@ -1,5 +1,11 @@
 variable "aws_region" {
   description = "Aws region"
-  type = string
-  default = "eu-central-1"
+  type        = string
+  default     = "eu-central-1"
+}
+
+variable "bucket_name" {
+  description = "Name of the bucket"
+  type        = string
+  default     = "dreamsync-app-ilona123"
 }
